f32ceff92dcc7ee65967d28f0cb6ad45fa3d4475
ABIN R PHILIP <abinrphilip34@gmail.com>
2026-06-04 11:51:40 +0530

    Day 8: Implement custom user model, roles, and bulletproof profile signals

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.users'
+
+    def ready(self):
+        import apps.users.signals
+
