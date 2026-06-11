da6bd5a98a8ce165e2bd68a11b3ee44bd0115a5a
ABIN R PHILIP <abinrphilip34@gmail.com>
2026-06-11 14:27:26 +0530

    Revert "Main"

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,31 +1,12 @@
-from rest_framework import serializers, permissions
-from rest_framework.permissions import AllowAny
-
+from rest_framework import serializers
 from .models import *
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = CustomUser
         fields = '__all__'
         read_only_fields = ('id',)
-
-
-    def create(self, validated_data ):
-            email = validated_data['email']
-            username = validated_data['username']
-            user = CustomUser.objects.create_user(
-                username=username,
-                email=email,
-                password=validated_data['password'],
-                role = validated_data.get('role','default'))
-
-            return user
-
-
-
 
 
 
@@ -33,18 +14,11 @@ class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
         fields = '__all__'
-        read_only_fields = ['id','user','is_active']
+        read_only_fields = ('id',)
 
-    def validate_experience_years(self, value):
-        if value < 0:
-            raise serializers.ValidationError("Experience years cannot be negative.")
-        if value > 50:
-            raise serializers.ValidationError("Please enter a valid number of years.")
-        return value
-
-class EmployerSerializer(serializers.ModelSerializer):
+class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employer
         fields = '__all__'
-        read_only_fields = ('id','user','is_active','is_verified')
+        read_only_fields = ('id',)
 
